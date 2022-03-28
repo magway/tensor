e13a51315af102de080fda6b49014e4d951b225a
magway <gmagway@gmail.com>
2022-03-28 16:20:54 +0200

    [+] TodoList component added + fetchJson util

---
+++ script/todo-list.js
@@ -0,0 +1,113 @@
+import fetchJson from "./utils/fetch-json.js";
+
+const BACKEND_URL = "https://jsonplaceholder.typicode.com"
+
+export default class TodoList {
+    element;
+    body;
+    userId = 0;
+    data = [];
+    loading = false;
+
+    constructor({
+        userID = 1
+    } = {}) {
+
+        this.userId = userID;
+        this.url = BACKEND_URL + `/users/${userID}/todos`;
+
+        this.render();
+    }
+
+    /**
+     * Render function: creates component DOM-node and stores it in the 'element' field
+     */
+    async render() {
+        const wrapper = document.createElement('div');
+
+        wrapper.innerHTML = this.getMarkup();
+
+        this.element = wrapper.firstElementChild;
+        this.body = this.element.querySelector('.todo-list__body');
+
+        this.setLoading(true);
+
+        //fetch some data asynchronously
+        const data = await fetchJson(this.url);
+
+        //for a better view let's leave 10 rows
+        this.renderRows(data.slice(0, 10));
+
+        this.setLoading(false);
+    }
+
+    /**
+     * Render function for component DOM-node creation
+     */
+    renderRows(data = []) {
+        if (data.length) {
+            this.element.classList.remove('todo-list--empty');
+            this.data = data;
+            this.body.innerHTML = this.getTableRows(data);
+        } else {
+            this.element.classList.add('todo-list--empty');
+        }
+    }
+
+    /**
+     * Return HTML of the list rows
+     *
+     * @param data
+     * @returns HTML
+     */
+    getTableRows(data) {
+        return data.map(({id, title, completed}) => `
+            <div class="todo-list__row">
+                <div class="todo-list__cell">${id}</div>
+                <div class="todo-list__cell">${title}</div>
+                <div class="todo-list__cell ${ completed ? 'success' : 'error'}">${ completed ? 'Done' : 'waiting'}</div>
+            </div>`
+        ).join('');
+    }
+
+    /**
+     * Static HTML for component
+     */
+    getMarkup() {
+        return `
+            <div class="todo-list">
+                <div class="todo-list__head">
+                    <div class="todo-list__row todo-list__row--header">
+                        <div class="todo-list__cell todo-list__cell--head">Id</div>
+                        <div class="todo-list__cell todo-list__cell--head">Title</div>
+                        <div class="todo-list__cell todo-list__cell--head">Status</div>
+                    </div>
+                </div>
+
+                <div class="todo-list__body"></div>
+
+                <div class="todo-list__placeholder">Loading...</div>
+
+                <div class="todo-list__empty-body">No items</div>
+            </div>
+        `
+    }
+
+    /**
+     * Set/Unset loading state of the list
+     * @param isLoading bool
+     */
+    setLoading(isLoading = false) {
+        this.loading = isLoading;
+        this.element.classList.toggle('todo-list--loading', isLoading);
+    }
+
+    /**
+     * Destroy list to clean up the memory.
+     * Just in case
+     */
+    destroy() {
+        this.element.remove();
+        this.body = null;
+    }
+}
---
+++ script/utils/fetch-json.js
@@ -0,0 +1,63 @@
+/**
+ * Same as 'fetch', but with error handler and response.json() call
+ * Throws our custom FetchError in case of errors.
+ * Handles network and json errors
+ */
+export default async function(url, params) {
+  let response;
+
+  // in case of network troubles
+  try {
+    response = await fetch(url, params);
+  } catch (err) {
+    throw new FetchError(response, 'Network error has occurred.');
+  }
+
+  let body;
+
+  // in case of bad response ( 299 < response.status < 200)
+  if (!response.ok) {
+    let errorText = response.statusText;
+
+    try {
+      body = await response.json();
+
+      errorText = (body.error && body.error.message)
+        || (body.data && body.data.error && body.data.error.message)
+        || errorText;
+    } catch (error) { /* ignore failed body */ }
+
+    let message = `Error ${response.status}: ${errorText}`;
+
+    throw new FetchError(response, body, message);
+  }
+
+  try {
+    return await response.json();
+  } catch (err) {
+    throw new FetchError(response, null, err.message);
+  }
+}
+
+/**
+ * Custom Error
+ */
+export class FetchError extends Error {
+  name = 'FetchError';
+
+  constructor(response, body, message) {
+    super(message);
+    this.response = response;
+    this.body = body;
+  }
+}
+
+/**
+ * Handle uncaught failed fetch
+ */
+window.addEventListener('unhandledrejection', event => {
+  if (event.reason instanceof FetchError) {
+    alert(event.reason.message);
+  }
+});
+
